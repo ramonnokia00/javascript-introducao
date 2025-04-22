70c7bbf392519cdcb2f9e7dbb6ef1db6613f3f94
unknown <ramonsantos6053@digitalcollege.internal>
2025-04-22 16:58:10 -0300

    javascript_basico_variantes

---
+++ index.js
@@ -0,0 +1,26 @@
+// let nomeSobrenome = "Ramon Soares"
+// let idade = 34;
+// console.log(idade);
+
+// // idade = idade + 1;
+// idade += 1;
+// console.log(idade);
+
+// // idade = idade * 2;
+// let numero = 2;
+// numero *= 2 * 2;
+// console.log(numero);
+// console.log(2 == 2);
+
+// let numero = 4;
+
+// console.log("2 == 2: ", numero == 2 && numero % 2 == 0);
+// console.log("2 == 2: ", numero == 2 || numero % 2 == 0);
+
+// let verdadeiro = true
+// console.log(verdadeiro);
+// console.log(!verdadeiro);
+// verdadeiro = !verdadeiro;
+// console.log(!verdadeiro);
+
+
