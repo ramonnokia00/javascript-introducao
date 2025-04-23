51f1e181e3e65b6fe1bfab46dd5f0415f711e33c
unknown <ramonsantos6053@digitalcollege.internal>
2025-04-23 16:44:56 -0300

    segundo commit sobre java

--- index.js
+++ index.js
@@ -1,26 +1,62 @@
-// let nomeSobrenome = "Ramon Soares"
-// let idade = 34;
-// console.log(idade);
+// Questao 1
+// let idadeMinima = 18;
 
-// // idade = idade + 1;
-// idade += 1;
-// console.log(idade);
+// if(idadeMinima >= 18){
+//     console.log("Maior de idade");
+// }else{
+//     console.log("menor de idade");
 
-// // idade = idade * 2;
-// let numero = 2;
-// numero *= 2 * 2;
-// console.log(numero);
-// console.log(2 == 2);
+// }
+// Questão 2
+// let numero = -10;
+// if(numero > 0){
+//     console.log("positivo");
+// } else if(numero == 0)
+//     {console.log("Zero");
+// } else{
+//     console.log("negativo");
+// }
+// questão 3
+// let numeroA = 5;
+// let numeroB = 5;
 
-// let numero = 4;
+// if (numeroA > numeroB) {
+//     console.log(`${numeroA} È maior que ${numeroB}`);
+// } else if (numeroB > numeroA){
+//     console.log(`${numeroB} é maior que ${numeroA}`);
+// } else {
+//     console.log("os numeros sao iguais");
+// }
+// Questão 4
+// let numero = 21;
+// if((numero % 2) ==0){
+//     console.log(`${numero} è par!`);
+// }else {
+//     console.log(`${numero} é impar!`);
 
-// console.log("2 == 2: ", numero == 2 && numero % 2 == 0);
-// console.log("2 == 2: ", numero == 2 || numero % 2 == 0);
+// }
+// Questão 5 
+// let notaA = 5;
+// let notaB = 5;
+// let notaC = 6;
+// let media = ((notaA + notaB + notaC) / 3).toFixed(1);
 
-// let verdadeiro = true
-// console.log(verdadeiro);
-// console.log(!verdadeiro);
-// verdadeiro = !verdadeiro;
-// console.log(!verdadeiro);
-
+// if (media >= 7) {
+//     situacao = "aprovado";
+// } else if (media > 5 && media < 7) {
+//     situacao = "Recuperação"
+// }else {
+//     situacao = "Reprovado"
+// }
+// console.log(`${situacao}, media: ${media}`);
+// questão 6
+// let valor = 620;
+// let desconto = 0.35;
+// if (valor >100 ){
+// console.log(`total a pagar R$ ${valor - (valor * desconto).toFixed(4)}`);
+// }else {
+//     console.log(`total a pagar R$ ${valor.toFixed(4)}`);
+// }
+// questão 7
+//Pulado
 
